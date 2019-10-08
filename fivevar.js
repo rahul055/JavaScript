8a05b0a9c413988ed043aa13096928d793064eef
rahul055 <rrjoshi055@gmail.com>
2019-10-08 21:29:57 +0530

    calculator

---
+++ fivevar.js
@@ -0,0 +1,26 @@
+var a = 10;
+var b = 20;
+ function addn()
+{
+	var c = a + b;
+	console.log(c);
+}
+function subs()
+{
+	var c = a - b;
+	console.log(c);
+}
+function mul()
+{
+	var c = a * b;
+	console.log(c);
+}
+function divid()
+{
+	var c = a / b;
+	console.log(c);
+}
+addn();
+subs();
+mul();
+divid();
