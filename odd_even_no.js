e4a310f41d828c6136beb7822be172a38d93ab03
rahul055 <56287458+rahul055@users.noreply.github.com>
2019-10-12 15:42:58 +0530

    Odd-even-no

---
+++ odd_even_no.js
@@ -0,0 +1,7 @@
+  var num = 8;
+         if(num % 5==0) {
+            console.log('Number is odd!');
+         } else {
+            console.log('Number is even!');
+         }
+         console.log(num);
