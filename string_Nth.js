eac0829df4a9df1a6b34aeb5bf244ac2f8fd61a0
rahul055 <rrjoshi055@gmail.com>
2019-10-17 23:02:42 +0530

    finding the Nth positing using BracketNotation

---
+++ string_Nth.js
@@ -0,0 +1,9 @@
+var mystr = "Beautiful";
+var myvar = mystr[0];
+console.log(myvar)
+var myvar2 = mystr[mystr.length - 1];
+console.log(myvar2)
+var myvar3 = mystr[mystr.length - 3]
+console.log(myvar3)
+var myvar4 = mystr[5]
+console.log(myvar4)
