1c29825c13d08326bbc0c41bea8b53e466fe2b0f
rahul055 <rrjoshi055@gmail.com>
2019-10-18 11:11:09 +0530

    shifting array with shift function

---
+++ array_shift.js
@@ -0,0 +1,16 @@
+var myarray = ["A", "B", "C"]
+
+var array2 = myarray.shift()
+
+console.log(myarray)
+console.log(array2)
+
+
+var myarray2 = [
+    ["rahul", 23],
+    ["rajshri", 25]
+]
+
+array2 = myarray2.shift()
+console.log(myarray2)
+console.log(array2)
