a22526d070cbe726d4aaa6b78c62dbba699f5795
rahul055 <rrjoshi055@gmail.com>
2019-10-18 10:49:05 +0530

    array pop function

---
+++ array_pop.js
@@ -0,0 +1,14 @@
+var myarray = [1, 2, 3, 4, 5, 10]
+var poped = myarray.pop()
+console.log(myarray)
+console.log(poped)
+
+var myarray2 = [
+    ["hi", "hello"],
+    ["how", "are you"],
+    ["and go out"]
+]
+
+var poped2 = myarray2.pop();
+console.log(myarray2)
+console.log(poped2)
