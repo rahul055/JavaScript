bd5894dfe92c9aee6a809311265c94ec3c51e913
rahul055 <rrjoshi055@gmail.com>
2019-10-18 10:39:15 +0530

    Array Push method

---
+++ array_push.js
@@ -0,0 +1,16 @@
+var myary = ["A", "B", "C", "D", ]
+
+
+myary.push("E", "F", "G");
+console.log(myary)
+
+
+var myary2 = [
+    ["Rahul", 23],
+    ["vaidyanath_college"]
+]
+
+myary2.push(["Parli-v", "beed"]
+
+)
+console.log(myary2)
