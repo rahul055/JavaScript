68e8a6e1ef8b77930d0a279a122a4d98b4b862f2
rahul055 <rrjoshi055@gmail.com>
2019-11-07 23:06:27 +0530

    function for pop and unshift values of array

--- funct.js
+++ funct.js
@@ -1,11 +1,28 @@
 function myfun(arr, val) {
     arr.push(val);
-    return arr.shift();
+    return arr.push();
 }
 
-
+function fun(ar, vl) {
+    ar.unshift(vl);
+    return ar.unshift()
+}
 myArr = [1, 2, 3, 4, 5];
 
-console.log("before" + myArr);
+console.log("before " + myArr);
 console.log(myfun(myArr, 6));
-console.log("after" + myArr);
+console.log("after " + myArr);
+
+ourarr = [2, 3, 4, 5, 6];
+
+console.log("before " + ourarr);
+console.log(fun(ourarr, 1));
+console.log('after ' + ourarr);
+
+
+arr = ['b', 'c', 'd', 'e', 'f', 'g']
+
+
+console.log("before " + arr);
+console.log(fun(arr, 'a'));
+console.log('after ' + arr);
---
+++ if_statement.js
@@ -0,0 +1,14 @@
+function (num) {
+a        return "number is less than 5";
+    } else if (num < 10) {
+        return "number is less than 10";
+    } else if (num < 15) {
+        return "number is less than 15";
+    } else if (num < 20) {
+        return "number is less than 20";
+    } else {
+        return "number is grater than 20";
+    }
+}
+
+console.log(function (4))
